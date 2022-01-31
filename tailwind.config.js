dd4a9b1140ed6f621be403937189af5d439305d3
carlos-angel <c.a.a.a.503@gmail.com>
2022-01-30 21:03:17 -0600

    feat: definir paleta de colores y el font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: (theme) => ({
+        ...theme('colors'),
+        primary: '#CC2D4A',
+        secondary: '#8FA206',
+        tertiary: '#61AEC9',
+      }),
+      textColor: {
+        primary: '#CC2D4A',
+        secondary: '#8FA206',
+        tertiary: '#61AEC9',
+      },
+      fontFamily: {
+        Montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
