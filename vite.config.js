f1156e8fce12f6788c4e6e686a4d4c669a55759a
carlos-angel <c.a.a.a.503@gmail.com>
2022-01-31 11:36:04 -0600

    feat: configurar alias

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      components: resolve('src/components/'),
+      screens: resolve('src/screens/'),
+      assets: resolve('src/assets/'),
+    },
+  },
+});
