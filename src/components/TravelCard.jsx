5b4f11722cefaa0d1a51503aab326d1a0ce3c0ff
carlos-angel <c.a.a.a.503@gmail.com>
2022-02-02 10:42:20 -0600

    refactor: crear componentes Body, Title y Description del TravelCard

--- src/components/TravelCard.jsx
+++ src/components/TravelCard.jsx
@@ -1,11 +1,26 @@
-export const TravelCard = ({ title, description, backgroundImage }) => {
+export const TravelCard = ({ children, className, backgroundImage = '' }) => {
   return (
     <div className='card'>
-      <div className={`w-full h-3/5 rounded-t-lg bg-cover bg-${backgroundImage}`}></div>
-      <div className='bg-secondary w-full h-2/5 text-white px-4 rounded-b-lg'>
-        <p className='font-bold text-xl pt-2'>{title}</p>
-        <p className='text-sm'>{description}</p>
-      </div>
+      <div
+        className={`${className ?? 'w-full h-3/5 rounded-t-lg bg-cover'} ${backgroundImage}`}
+      ></div>
+      {children}
     </div>
   );
 };
+
+const Body = ({ children, className }) => (
+  <div className={`w-full h-2/5 px-4 ${className ?? 'rounded-b-lg bg-secondary text-white'}`}>{children}</div>
+);
+
+const Title = ({ title, className }) => (
+  <p className={className ?? 'font-bold text-xl pt-2'}>{title}</p>
+);
+
+const Description = ({ description, className }) => (
+  <p className={className ?? 'text-sm'}>{description}</p>
+);
+
+TravelCard.Title = Title;
+TravelCard.Description = Description;
+TravelCard.Body = Body;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,15 +2,36 @@ import { Hero } from 'components/Hero';
 import { TravelCard } from 'components/TravelCard';
 
 export const Home = () => {
-  console.log('entre');
   return (
     <>
       <nav></nav>
       <div id='tab_bar'></div>
       <section className='w-full h-screen'>
         <Hero />
-        <div id='recomendadas' className='p-20'>
-          <TravelCard title='Norway' description='Paisajes Increíbles' backgroundImage='norway' />
+        <div id='recomendados' className='p-6'>
+          <p className='text-3xl font-semibold text-primary'>Recomendados</p>
+          <div className=' w-auto h-72 items-center mt-6 overflow-x-auto overscroll-x-contain flex space-x-4'>
+            <TravelCard backgroundImage='bg-norway'>
+              <TravelCard.Body>
+                <TravelCard.Title title='Norway' />
+                <TravelCard.Description description='Paisajes Increíbles' />
+              </TravelCard.Body>
+            </TravelCard>
+
+            <TravelCard backgroundImage='bg-new_york'>
+              <TravelCard.Body className="bg-white">
+                <TravelCard.Title title='New York' className="text-sky-400 font-bold text-xl pt-2" />
+                <TravelCard.Description description='Paisajes Increíbles' className="text-sm text-sky-400" />
+              </TravelCard.Body>
+            </TravelCard>
+
+            <TravelCard backgroundImage='bg-chicago'>
+              <TravelCard.Body>
+                <TravelCard.Title title='Chicago' />
+                <TravelCard.Description description='Paisajes Increíbles' />
+              </TravelCard.Body>
+            </TravelCard>
+          </div>
         </div>
         <div id='rentas_destacadas'></div>
       </section>
