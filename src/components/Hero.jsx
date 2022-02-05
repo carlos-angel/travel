811960721cb2564e707a0f4e09467e59616af471
carlos-angel <c.a.a.a.503@gmail.com>
2022-02-05 01:03:13 -0600

    style: aumentar el tamaño del Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ export const Hero = ({ children }) => {
   return (
     <div
       id='home'
-      className='w-full bg-center bg-cover h-128 bg-sanFrancisco lg:bg-sanFranciscoDesktop'
+      className='w-full bg-center bg-cover h-3/4 bg-sanFrancisco lg:bg-sanFranciscoDesktop'
     >
       <div className='absolute flex flex-col items-center w-full h-full py-4 space-y-96 lg:space-y-0 lg:items-start lg:pt-48 lg:justify-start'>
         {children}
